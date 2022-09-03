0f4471a71c50486f92d2f79ab11c5c9ab28c18cd
01NakulGoel <nakulgoyal78@gmail.com>
2022-09-03 16:05:07 +0530

    new

---
+++ Assignment-Mathematical-Operator/Problem2.js
@@ -0,0 +1,12 @@
+let one=1
+let two= 2
+let three= 3
+let four=4 
+let five=5
+// Find the sum of the values stored in one, two, three, four, after the above operation are performed
+one = one*2
+two=two*2
+three= three*2
+four=four*2
+five=five*2
+console.log(one+two+three+four+five)
