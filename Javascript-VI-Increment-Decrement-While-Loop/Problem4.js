9bbec69b5f8644cfb7d4363c0b789da918e53803
Nakul Goel <112364649+01NakulGoel@users.noreply.github.com>
2022-09-05 15:32:33 +0530

    Update Problem4.js
    average

--- Javascript-VI-Increment-Decrement-While-Loop/Problem4.js
+++ Javascript-VI-Increment-Decrement-While-Loop/Problem4.js
@@ -1,8 +1,12 @@
-let average =0 
-let x=0
-while (x<=100){
-  average= average+x
+let average = 0
+let x = 2
+let count = 0
+while (x <= 100) {
+  average = average + x
+  count++
 
-  x= x+2
+  x = x + 2
 }
-console.log(average/2)
+
+
+console.log(average / count)
