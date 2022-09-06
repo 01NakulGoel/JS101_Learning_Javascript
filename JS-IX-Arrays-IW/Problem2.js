85123163cbae2f7ec03cd8fd269cc966b1f940e0
Nakul Goel <112364649+01NakulGoel@users.noreply.github.com>
2022-09-06 16:06:24 +0530

    new method

--- JS-IX-Arrays-IW/Problem2.js
+++ JS-IX-Arrays-IW/Problem2.js
@@ -1,2 +1,18 @@
-let a = 'rahul'
-console.log(a.toUpperCase())
+let input = ['b', 'a']
+let small = 'abcdefghijklmnopqrstuvwxyz'
+let upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+let smallCase = [...small]
+let upperCase = [...upper]
+for (let i = 0; i < upperCase.length; i++) {
+  for (let j = 0; j < upperCase.length; j++) {
+    if (input[i] === smallCase[j]) {
+      input[i] = upperCase[j]
+    }
+  }
+}
+console.log(input)
+
+
+// let a = 'rahul'
+// console.log(a.toUpperCase())
